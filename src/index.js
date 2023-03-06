f48439cece2339628105b412b468c323bb569c34
Tona <tona@mango-byte.com>
2023-03-06 15:17:28 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+function Root(props) {
+    return (
+        <h4>Hello World</h4>
+    );
+}
+
+ReactDOM.render(<Root />, document.getElementById('root'));
